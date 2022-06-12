9a1b721072f75384cc348c06fd2a229e4461e8b9
Jason Telfer <jason@jasont.dev>
2022-06-12 19:10:12 +1000

    /hello endpoint secured with Cognito

---
+++ src/main/java/xyz/taskmesh/api/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package xyz.taskmesh.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests().anyRequest().authenticated().and()
+                .httpBasic().disable().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt).build();
+    }
+}
