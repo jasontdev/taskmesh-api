950663ec1d4f98e05d569a781d3a65ff13f3eaa9
Jason Telfer <jason@jasont.dev>
2022-06-17 05:50:38 +1000

    application.yml config for DynamoDB tablename and endpoint

--- src/main/java/xyz/taskmesh/api/config/DynamoDBConfiguration.java
+++ src/main/java/xyz/taskmesh/api/config/DynamoDBConfiguration.java
@@ -1,17 +1,24 @@
 package xyz.taskmesh.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.net.URI;
+
 @Configuration
 public class DynamoDBConfiguration {
 
+    @Value("$(dynamodb.endpoint)")
+    private String endpoint;
+
     @Bean
     public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
-        final var dynamoDbClient = DynamoDbClient.builder().region(Region.of("ap-southeast-2")).build();
+        final var dynamoDbClient = DynamoDbClient.builder().region(Region.of("ap-southeast-2"))
+                .endpointOverride(URI.create(endpoint)).build();
         return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
     }
 }
--- src/main/java/xyz/taskmesh/api/repository/TaskRepository.java
+++ src/main/java/xyz/taskmesh/api/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package xyz.taskmesh.api.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
@@ -11,13 +12,16 @@ import java.util.UUID;
 @Service
 public class TaskRepository {
 
+    @Value("$(dynamodb.tablename)")
+    private String tablename;
+
     @Autowired
     DynamoDbEnhancedClient dynamoDbEnhancedClient;
 
     public Task save(Task task) {
         // should check to make sure task has a tasklistId
         task.setTaskId(UUID.randomUUID().toString());
-        dynamoDbEnhancedClient.table("tablename", TableSchema.fromBean(Task.class)).putItem(task);
+        dynamoDbEnhancedClient.table(tablename, TableSchema.fromBean(Task.class)).putItem(task);
 
         return task;
     }
