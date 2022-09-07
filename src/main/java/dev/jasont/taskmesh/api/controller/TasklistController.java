fa732f7e713b1d208b0664d2e6928aaf32ae340a
Jason T <jason@jasont.dev>
2022-09-07 17:37:21 +1000

    Removed repositories from TasklistController

--- src/main/java/dev/jasont/taskmesh/api/controller/TasklistController.java
+++ src/main/java/dev/jasont/taskmesh/api/controller/TasklistController.java
@@ -1,32 +1,23 @@
 package dev.jasont.taskmesh.api.controller;
 
-import dev.jasont.taskmesh.api.entity.Tasklist;
-import dev.jasont.taskmesh.api.entity.User;
-import dev.jasont.taskmesh.api.repository.TaskRepository;
-import dev.jasont.taskmesh.api.repository.TasklistRepository;
-import dev.jasont.taskmesh.api.repository.UserRepository;
-import dev.jasont.taskmesh.api.service.TasklistService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.jasont.taskmesh.api.entity.Tasklist;
+import dev.jasont.taskmesh.api.service.TasklistService;
 
 @RestController
 public class TasklistController {
-    private TasklistRepository tasklistRepository;
-    private UserRepository userRepository;
-    private TaskRepository taskRepository;
     private TasklistService tasklistService;
 
-    public TasklistController(@Autowired TasklistRepository tasklistRepository,
-                              @Autowired UserRepository userRepository,
-                              @Autowired TaskRepository taskRepository,
-                              @Autowired TasklistService tasklistService) {
-        this.tasklistRepository = tasklistRepository;
-        this.userRepository = userRepository;
-        this.taskRepository = taskRepository;
+    public TasklistController(@Autowired TasklistService tasklistService) {
         this.tasklistService = tasklistService;
     }
 
@@ -36,7 +27,7 @@ public class TasklistController {
     }
 
     @GetMapping("/tasklist/{id}")
-    public ResponseEntity<Tasklist> getTasklist(@PathVariable("id") Long tasklistId) {
-        return ResponseEntity.of(tasklistRepository.findById(tasklistId));
+    public ResponseEntity<Tasklist> getTasklist(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal accessToken, @PathVariable("id") Long tasklistId) {
+        return ResponseEntity.of(tasklistService.getById(accessToken, tasklistId));
     }
 }
